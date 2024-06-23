04c94783df8bd7f8f7cf0ebe9243f237eb545014
zidankhan86 <zidankhan718@gmail.com>
2024-06-24 00:58:08 +0600

    feat:sidebar icon

--- resources/views/backend/components/blog/blogList.blade.php
+++ resources/views/backend/components/blog/blogList.blade.php
@@ -2,7 +2,11 @@
     <br>
     <h2 style="text-align: center">Blog Table</h2>
     <div style="text-align: right">
-        <a href="{{ route('blog.form') }}" class="btn btn-info" style="margin-right: 10px;">+ Add Blog</a>
+
+        <a href="{{ route('category.list') }}" class="btn btn-primary" style="margin-right: 10px;"><span><x-backend.svg.plus-icon/></span>Category</a>
+
+        <a href="{{ route('blog.form') }}" class="btn btn-info" style="margin-right: 10px;"><span><x-backend.svg.plus-icon/></span>Add Blog</a>
+
     </div>
     <br><br>
     <div class="col-12">
@@ -21,33 +25,34 @@
                     </thead>
                     <tbody>
                         @foreach ($blogs as $blog)
-                        <tr>
-                            <td data-label="Image" class="align-middle">
-                                @foreach(unserialize($blog->post_image) as $image)
-                                <div class="d-inline-block" style="margin-right: 10px;">
-                                    <img src="{{ url('/uploads/' . $image) }}" alt="Image" class="img-thumbnail"
-                                        style="max-width: 100px; max-height: 100px;">
-                                </div>
-                                @endforeach
-                            </td>
-                            <td data-label="Title" class="align-middle">{{ $blog->title }}</td>
-                            <td data-label="Category" class="align-middle">{{ $blog->category->type_name }}</td>
-                            <td data-label="Status" class="align-middle">{{ $blog->status == 1 ? 'Active':'Inactive' }}</td>
-                            <td data-label="Date" class="align-middle">{{ $blog->created_at->format('D-M-Y') }}</td>
-                            <td data-label="Action" class="align-middle">
-                                <div class="btn-list flex-nowrap">
-                                    <a href="#" class="btn btn-info">Edit</a>
-                                    <div class="dropdown">
-                                        <button class="btn btn-secondary dropdown-toggle align-text-top"
-                                            data-bs-toggle="dropdown">Actions</button>
-                                        <div class="dropdown-menu dropdown-menu-end">
-                                            <a class="dropdown-item" href="#">Action</a>
-                                            <a class="dropdown-item" href="#">Another action</a>
+                            <tr>
+                                <td data-label="Image" class="align-middle">
+                                    @foreach (unserialize($blog->post_image) as $image)
+                                        <div class="d-inline-block" style="margin-right: 10px;">
+                                            <img src="{{ url('/uploads/' . $image) }}" alt="Image"
+                                                class="img-thumbnail" style="max-width: 100px; max-height: 100px;">
+                                        </div>
+                                    @endforeach
+                                </td>
+                                <td data-label="Title" class="align-middle">{{ $blog->title }}</td>
+                                <td data-label="Category" class="align-middle">{{ $blog->category->type_name }}</td>
+                                <td data-label="Status" class="align-middle">
+                                    {{ $blog->status == 1 ? 'Active' : 'Inactive' }}</td>
+                                <td data-label="Date" class="align-middle">{{ $blog->created_at->format('D-M-Y') }}</td>
+                                <td data-label="Action" class="align-middle">
+                                    <div class="btn-list flex-nowrap">
+                                        <a href="#" class="btn btn-info">Edit</a>
+                                        <div class="dropdown">
+                                            <button class="btn btn-secondary dropdown-toggle align-text-top"
+                                                data-bs-toggle="dropdown">Actions</button>
+                                            <div class="dropdown-menu dropdown-menu-end">
+                                                <a class="dropdown-item" href="#">Action</a>
+                                                <a class="dropdown-item" href="#">Another action</a>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            </td>
-                        </tr>
+                                </td>
+                            </tr>
                         @endforeach
                     </tbody>
                 </table>
--- resources/views/backend/components/fixed/sidebar.blade.php
+++ resources/views/backend/components/fixed/sidebar.blade.php
@@ -150,7 +150,7 @@
               </span>
             </a>
           </li>
-          <li class="nav-item dropdown">
+          {{-- <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" data-bs-auto-close="false" role="button" aria-expanded="false" >
               <span class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/package -->
                 <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 3l8 4.5l0 9l-8 4.5l-8 -4.5l0 -9l8 -4.5" /><path d="M12 12l8 -4.5" /><path d="M12 12l0 9" /><path d="M12 12l-8 -4.5" /><path d="M16 5.25l-8 4.5" /></svg>
@@ -167,14 +167,14 @@
                   </a>
               </div>
             </div>
-          </li>
+          </li> --}}
 
 
 
           <li class="nav-item ">
             <a class="nav-link" href="{{ route('category.list') }}"  data-bs-auto-close="false" role="button" aria-expanded="false" >
               <span class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/package -->
-                <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 3l8 4.5l0 9l-8 4.5l-8 -4.5l0 -9l8 -4.5" /><path d="M12 12l8 -4.5" /><path d="M12 12l0 9" /><path d="M12 12l-8 -4.5" /><path d="M16 5.25l-8 4.5" /></svg>
+                <x-backend.svg.category-icon/>
               </span>
               <span class="nav-link-title">
                 Category
@@ -185,7 +185,7 @@
           <li class="nav-item ">
             <a class="nav-link" href="{{ route('blog.list') }}"  data-bs-auto-close="false" role="button" aria-expanded="false" >
               <span class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/package -->
-                <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 3l8 4.5l0 9l-8 4.5l-8 -4.5l0 -9l8 -4.5" /><path d="M12 12l8 -4.5" /><path d="M12 12l0 9" /><path d="M12 12l-8 -4.5" /><path d="M16 5.25l-8 4.5" /></svg>
+               <x-backend.svg.blog-icon/>
               </span>
               <span class="nav-link-title">
                 Blog
@@ -197,7 +197,7 @@
           <li class="nav-item ">
             <a class="nav-link" href="#"  data-bs-auto-close="false" role="button" aria-expanded="false" >
               <span class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/package -->
-                <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 3l8 4.5l0 9l-8 4.5l-8 -4.5l0 -9l8 -4.5" /><path d="M12 12l8 -4.5" /><path d="M12 12l0 9" /><path d="M12 12l-8 -4.5" /><path d="M16 5.25l-8 4.5" /></svg>
+                <x-backend.svg.about-icon/>
               </span>
               <span class="nav-link-title">
                 About
@@ -208,7 +208,7 @@
           <li class="nav-item ">
             <a class="nav-link" href="{{ route('user.list') }}"  data-bs-auto-close="false" role="button" aria-expanded="false" >
               <span class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/package -->
-                <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 3l8 4.5l0 9l-8 4.5l-8 -4.5l0 -9l8 -4.5" /><path d="M12 12l8 -4.5" /><path d="M12 12l0 9" /><path d="M12 12l-8 -4.5" /><path d="M16 5.25l-8 4.5" /></svg>
+                <x-backend.svg.user-icon/>
               </span>
               <span class="nav-link-title">
                 User
