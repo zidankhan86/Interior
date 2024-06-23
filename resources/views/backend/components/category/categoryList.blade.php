17d29ef90a9d45130f245af1c9a3d1e20950eea5
zidankhan86 <zidankhan718@gmail.com>
2024-06-24 01:17:56 +0600

    fix: dashboard and others

--- resources/views/backend/components/category/categoryList.blade.php
+++ resources/views/backend/components/category/categoryList.blade.php
@@ -1,59 +1,72 @@
-
-
 <div class="container">
-    <br><h2 style="text-align: center">Category Table</h2>
-    <div style="text-align: right">
-        <a href="{{ route('category.form') }}" class="btn btn-info" style="margin-right: 10px;">+ Add Category</a>
-    </div><br><br>
-    <div class="col-12">
-        <div class="card">
-          <div class="table-responsive">
-            <table
-    class="table table-vcenter table-mobile-md card-table">
-              <thead>
-                <tr>
-                    <th>Id</th>
-                  <th>Category Type</th>
-                  <th class="w-1">Action</th>
-                </tr>
-              </thead>
-              <tbody>
-
-                @foreach ($categories as $category)
-                <tr>
-                    <tr>
-                        <td>{{  $category->id }}</td>
-                        <td data-label="Name" >{{  $category->type_name }}</td>
-
-
-                  <td>
-                    <div class="btn-list flex-nowrap">
-                      <a href="#" class="btn">
-                        Edit
-                      </a>
-                      <div class="dropdown">
-                        <button class="btn dropdown-toggle align-text-top" data-bs-toggle="dropdown">
-                          Actions
-                        </button>
-                        <div class="dropdown-menu dropdown-menu-end">
-                          <a class="dropdown-item" href="#">
-                            Action
-                          </a>
-                          <a class="dropdown-item" href="#">
-                            Another action
-                          </a>
-                        </div>
-                      </div>
-                    </div>
-                  </td>
-                </tr>
-
-                @endforeach
-
-              </tbody>
-            </table>
+  <br><h2 style="text-align: center">Category Table</h2>
+  <div class="row">
+      <!-- Category Table Column -->
+      <div class="col-md-8">
+          <div class="card">
+              <div class="table-responsive">
+                  <table class="table table-vcenter table-mobile-md card-table">
+                      <thead>
+                          <tr>
+                              <th>Id</th>
+                              <th>Category Type</th>
+                              <th class="w-1">Action</th>
+                          </tr>
+                      </thead>
+                      <tbody>
+                          @foreach ($categories as $category)
+                          <tr>
+                              <td>{{ $category->id }}</td>
+                              <td data-label="Name">{{ $category->type_name }}</td>
+                              <td>
+                                  <div class="btn-list flex-nowrap">
+                                      <a href="#" class="btn">Edit</a>
+                                      <div class="dropdown">
+                                          <button class="btn dropdown-toggle align-text-top" data-bs-toggle="dropdown">
+                                              Actions
+                                          </button>
+                                          <div class="dropdown-menu dropdown-menu-end">
+                                              <a class="dropdown-item" href="#">Action</a>
+                                              <a class="dropdown-item" href="#">Another action</a>
+                                          </div>
+                                      </div>
+                                  </div>
+                              </td>
+                          </tr>
+                          @endforeach
+                      </tbody>
+                  </table>
+              </div>
           </div>
-        </div>
       </div>
 
+      <!-- Category Form Column -->
+      <div class="col-md-4">
+          <form action="{{ route('category.store') }}" method="post">
+              @csrf
+              <div class="card">
+                  <div class="card-body">
+                      <h3 class="card-title">Category Form</h3>
+                      <div class="mb-3">
+                          <label class="form-label">Type Name</label>
+                          <input type="text" name="type_name" class="form-control" placeholder="Type Name">
+                          @if ($errors->has('type_name'))
+                          <p class="text-danger">{{ $errors->first('type_name') }}</p>
+                          @endif
+                      </div>
+                      <div class="mb-3">
+                          <label class="form-label">Status</label>
+                          <select name="status" class="form-control">
+                              <option value="1">Active</option>
+                              <option value="0">Inactive</option>
+                          </select>
+                      </div>
+                  </div>
+                  <div class="card-footer text-end">
+                      <button type="submit" class="btn btn-primary">Create Category</button>
+                  </div>
+              </div>
+          </form>
+      </div>
+  </div>
 </div>
