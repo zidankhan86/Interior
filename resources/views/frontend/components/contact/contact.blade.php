630d6e7ace162f4cf1d748a5d2221f5f979f1016
zidankhan86 <zidankhan718@gmail.com>
2024-12-10 22:59:13 +0600

    ..

--- resources/views/frontend/components/contact/contact.blade.php
+++ resources/views/frontend/components/contact/contact.blade.php
@@ -1,12 +1,12 @@
 
 
-<section class="ftco-section contact-section">
+<br><br><section class="ftco-section contact-section">
     <div class="container">
       <div class="row d-flex mb-5 contact-info">
         <div class="col-md-12 mb-4">
           <h2 class="h4">Contact Information</h2>
         </div>
-        
+
       </div>
       <div class="row block-9">
         <div class="col-md-6 order-md-last pr-md-5">
@@ -28,22 +28,22 @@
               <input type="submit" class="btn btn-primary py-3 px-5">
             </div>
           </form>
-        
+
         </div>
 
         <div class="col-md-6">
             <div class="w-100"></div>
         <div class="col-md-6">
-          <p><span>Address:</span> 198 West 21th Street, Suite 721 New York NY 10016</p>
+          <p><span>Address:</span> Road:23, sector:10, Dhaka, Bangladesh</p>
         </div>
         <div class="col-md-6">
-          <p><span>Phone:</span> <a href="tel://1234567920">+ 1235 2355 98</a></p>
+          <p><span>Phone:</span> <a href="tel://01768-810999">01768-810999</a></p>
         </div>
         <div class="col-md-6">
-          <p><span>Email:</span> <a href="mailto:info@yoursite.com">info@yoursite.com</a></p>
+          <p><span>Email:</span> <a href="mailto:info@yoursite.com">innards.ds@gmail.com</a></p>
         </div>
         <div class="col-md-6">
-          <p><span>Website</span> <a href="#">yoursite.com</a></p>
+          <p><span>Website</span> <a href="https://innardsds.com/">innardsds.com</a></p>
         </div>
         </div>
       </div>
--- resources/views/frontend/layout/app.blade.php
+++ resources/views/frontend/layout/app.blade.php
@@ -1,42 +1,36 @@
 
 
-
-
-
-
-
-
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <title>{{ env('app.name') }}</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    
+
  {{-- CSS --}}
  @include('frontend.components.fixed.style')
   </head>
   <body>
-    
+
 	  {{-- CSS --}}
     @include('frontend.components.fixed.header')
 
     {{-- Content --}}
     @yield('content')
 
-    
+
     {{-- Footer --}}
     @include('frontend.components.fixed.footer')
-    
-  
+
+
 
   <!-- loader -->
   <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
 
 
-  
+
     {{-- Js --}}
     @include('frontend.components.fixed.script')
-    
+
   </body>
 </html>
