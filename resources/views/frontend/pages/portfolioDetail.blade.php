b0be0e1671b69ebfc5d06de1808cd2f2a9862599
zidankhan86 <zidankhan718@gmail.com>
2024-12-23 23:20:32 +0600

    fix: cdn issue

--- resources/views/frontend/pages/portfolioDetail.blade.php
+++ resources/views/frontend/pages/portfolioDetail.blade.php
@@ -5,3 +5,6 @@
 @include('frontend.components.project.portfolio_detaills');
 
 @endsection
+<!-- Include Bootstrap CSS and JavaScript -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
